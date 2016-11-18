633dd20679364f5f5dbd45859d093436ecd0830b
J Ridgeway <sw333zy@gmail.com>
2016-11-18 15:52:54 -0500

    todays non progress

--- src/js/data.js
+++ src/js/data.js
@@ -1,4 +1,45 @@
 (function() {
   'use strict';
-  
+
+      window.github = window.github || {};
+      window.github.search = search;
+      window.github.get = get;
+
+
+        /**
+         * @param {String} query search github repositories
+         * @param {String} token a user token required to do a search
+         * @return {Promise}
+         */
+        function search(query, token) {
+
+         return $.ajax({
+            url: 'https://api.github.com/search/repositories?q=' + query,
+            method: 'GET',
+            dataType: 'json',
+            headers: {
+              authorization: 'token ' + token
+            }
+          });
+
+        }
+
+        console.log(search);
+
+        function get(token, url) {
+          return $.ajax({
+            url: url.split('{')[0],
+            method: 'GET',
+            dataType: 'json',
+            headers: {
+              authorization: 'token ' + token
+            }
+          });
+        }
+
+        console.log(get)
+
+
+
+
 }());
--- src/js/ui.js
+++ src/js/ui.js
@@ -1,4 +1,30 @@
 (function() {
-  'use strict';
-  
+    'use strict';
+
+    window.github = window.github || {};
+
+
+    $('#search')
+        .on('submit', function find(event) {
+            event.preventDefault();
+
+            var token = $('.api').val();
+            console.log(token)
+            var query = $('.query').val();
+        })
+
+    .then(function handleSuccess(data) {
+        console.log('success', data);
+
+        var randomRepository = data.items;
+        var randomSelector = randomRepository[Math.ceil(Math.random() * randomRepository.length)]
+        var url = randomSelector.repos_url;
+        var a = window.github.get(token, url);
+        return a;
+    })
+
+    .fail(function handleSuccess(xhr) {
+        console.log('failure', xhr)
+    });
+
 }());
